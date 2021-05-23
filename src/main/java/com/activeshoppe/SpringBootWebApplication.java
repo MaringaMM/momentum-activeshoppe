0172b04875632af275fe034b9964b84c365f6bd2
Musa Maringa <MusaM@BankservAfrica.com>
2021-05-24 01:00:34 +0200

    swagger2 added for api documentation

--- src/main/java/com/activeshoppe/SpringBootWebApplication.java
+++ src/main/java/com/activeshoppe/SpringBootWebApplication.java
@@ -1,10 +1,12 @@
-package com.activeshoppe;
+ package com.activeshoppe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan(basePackages = {"com.activeshoppe.services", "com.activeshoppe.entities", "com.activeshoppe.repositories", "com.activeshoppe.controller"})
 public class SpringBootWebApplication {
 
