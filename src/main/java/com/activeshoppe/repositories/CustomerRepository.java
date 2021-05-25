64f47c8eebb627978ec89ac976e4ffdfe041e8a5
Musa Maringa <MusaM@BankservAfrica.com>
2021-05-25 11:43:34 +0200

    exception fix

--- src/main/java/com/activeshoppe/repositories/CustomerRepository.java
+++ src/main/java/com/activeshoppe/repositories/CustomerRepository.java
@@ -4,16 +4,14 @@
  * and open the template in the editor.
  */
 package com.activeshoppe.repositories;
+
+
 import com.activeshoppe.entities.Customer;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author MusaM
  */
-@Repository("customerRepository")
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 }
--- src/main/java/com/activeshoppe/repositories/ProductRepository.java
+++ src/main/java/com/activeshoppe/repositories/ProductRepository.java
@@ -2,8 +2,7 @@ package com.activeshoppe.repositories;
 
 import com.activeshoppe.entities.Product;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository("productRepository") //ADDED
+
 public interface ProductRepository extends CrudRepository<Product, Integer>{
 }
